3617c47e25f8d5554ad782e745f126719837ee7d
Eddyalexdev <eddyalex.dev@gmail.com>
2022-12-25 16:51:20 -0400

    se termino la funcionalidad de encriptacion

--- js/encriptar.js
+++ js/encriptar.js
@@ -1,11 +1,31 @@
 let text = document.getElementById('text')
 let buttonEncrypt = document.getElementById('b-encrypt')
 let buttonDesEncrypt = document.getElementById('b-desencrypt')
+let textContainer = document.getElementById('text-container')
+let notContent = document.getElementById('not-content')
 
 buttonEncrypt.addEventListener('click', encriptar)
 
+const letters = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat"
+}
+
 function encriptar() {
-    console.log(text.value)
+    const value = text.value
+    if (value == "") return 
+    const textValues = value.split('')
+    const array = textValues.map((letter) => 
+        letters[letter] != undefined ? 
+            letter.replace(letter, letters[letter]) : 
+            letter
+    )
+    textContainer.textContent = array.join('')
+    notContent.style.display = 'none'
+
 }
 
 
