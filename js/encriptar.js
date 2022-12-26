e572cf83829f98ac5d634c9daf2f250540b1df02
Eddyalexdev <eddyalex.dev@gmail.com>
2022-12-25 20:04:42 -0400

    se agregaron los ultimos detalles

--- js/encriptar.js
+++ js/encriptar.js
@@ -5,9 +5,6 @@ let textContainer = document.getElementById('text-container')
 let notContent = document.getElementById('not-content')
 let textContent = document.getElementById('text-content')
 
-buttonEncrypt.addEventListener('click', encriptar)
-buttonDesEncrypt.addEventListener('click', desencriptar)
-
 const encryptLetters = {
     a: "ai",
     e: "enter",
@@ -16,13 +13,9 @@ const encryptLetters = {
     u: "ufat",
 }
 
-const desencryptLetters = {
-    ai: "a",
-    enter: "e",
-    imes: "i",
-    ober: "o",
-    ufat: "u"
-}
+buttonEncrypt.addEventListener('click', encriptar)
+buttonDesEncrypt.addEventListener('click', desencriptar)
+
 
 function encriptar() {
     const value = text.value
@@ -41,7 +34,11 @@ function encriptar() {
 function desencriptar() {
     const value = text.value
     if (value == "") return 
-    const newValue = value.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u')
+    const newValue = value.replaceAll('ai', 'a')
+                    .replaceAll('enter', 'e')
+                    .replaceAll('imes', 'i')
+                    .replaceAll('ober', 'o')
+                    .replaceAll('ufat', 'u')
     textContainer.textContent = newValue
     notContent.style.display = 'none'
     textContent.style.display = 'flex'
